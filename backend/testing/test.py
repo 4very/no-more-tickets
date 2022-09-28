818ed12730f0a35356fc4e59f5d84ac2ea2e2bec
Avery Sommer <asommer999@gmail.com>
2022-09-28 16:23:54 -0400

    added to last commit

--- backend/testing/test.py
+++ backend/testing/test.py
@@ -1,11 +1,10 @@
-
 from OSMPythonTools.api import Api
 from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
 import logging
 
 api = Api()
 overpass = Overpass()
-logging.getLogger('OSMPythonTools').setLevel(logging.ERROR)
+logging.getLogger("OSMPythonTools").setLevel(logging.ERROR)
 
 # print(.tag('border_type'))
 # print(api.query('way/669246263').nodes())
@@ -15,10 +14,11 @@ road = "Congress Street"
 bounds = ["4th Street", "5th Street"]
 
 
-
-query = overpassQueryBuilder(area='relation/174387', elementType='way', selector=f'"name"~"{road}"')
+query = overpassQueryBuilder(
+    area="relation/174387", elementType="way", selector=f'"name"~"{road}"'
+)
 result = overpass.query(query)
 for way in result.ways():
-  for node in way.nodes(shallow=False):
-    api.query(f'node/{node.id()}')
-    print(node.id(), node.members())
+    for node in way.nodes(shallow=False):
+        api.query(f"node/{node.id()}")
+        print(node.id(), node.members())
