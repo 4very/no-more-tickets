9672a0700560121993ad69e83346ed528898b7fe
Avery Sommer <asommer999@gmail.com>
2022-09-28 17:36:42 -0400

    split tailwind config to sep file

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -29,22 +29,5 @@ export default defineNuxtConfig({
       },
       exclude: ['node_modules', '.nuxt', 'dist']
     }
-  },
-  tailwindcss: {
-    config: {
-      content: [
-        'components/**/*.{vue,js}',
-        'layouts/**/*.vue',
-        'pages/**/*.vue',
-        'composables/**/*.{js,ts}',
-        'plugins/**/*.{js,ts}',
-        'App.{js,ts,vue}',
-        'app.{js,ts,vue}'
-      ],
-      theme: {
-        extend: {}
-      },
-      plugins: []
-    }
   }
 });
