28e4f955de84067cbc157104756719e3bbe94f7b
Avery Sommer <asommer999@gmail.com>
2022-09-30 13:08:07 -0400

    final testing of osm queries

--- backend/testing/test.py
+++ backend/testing/test.py
@@ -9,16 +9,17 @@ overpass = Overpass()
 road = "Division Street"
 bounds = ["4th Street", "1st Street"]
 
-query = f'''
+query = f"""
 way[highway][name="{road}"];node(w)->.n1;
 way[highway][name="{bounds[0]}"];node(w)->.n2;
 way[highway][name="{bounds[1]}"];node(w)->.n3;
 (node.n1.n2; node.n1.n3;);
-out body;'''
+out body;"""
 
-bbox = {'bbox': "42.6940400, -73.7064390, 42.7952720, -73.6491630"}
+bbox = {"bbox": "42.6940400, -73.7064390, 42.7952720, -73.6491630"}
 
 from pprint import pprint
+
 coords = overpass.query(query, settings=bbox).elements()
 
 lats = []
@@ -26,13 +27,13 @@ lons = []
 for elt in coords:
     lats.append(elt.lat())
     lons.append(elt.lon())
-    
+
 bbox2 = {"bbox": f"{min(lats)}, {min(lons)}, {max(lats)}, {max(lons)}"}
 
-query2 = f'''
+query2 = f"""
 way[highway][name="{road}"];node(w)->.n1;
 node.n1;
 out body;
-'''
+"""
 
 pprint(overpass.query(query2, settings=bbox2).toJSON())
